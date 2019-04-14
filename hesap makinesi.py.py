6a0308a861e8c34155e5a41c611cb813a2c6aae6
XovoXtht <49613271+XovoXtht@users.noreply.github.com>
2019-04-14 21:44:08 +0300

    Add files via upload

---
+++ hesap makinesi.py.py
@@ -0,0 +1,36 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> while True:
+    secenek1 = "(1) toplama"
+    secenek2 = "(2) cikarma"
+    secenek3 = "(3) carpma"
+    secenek4 = "(4) bolme"
+
+    print "Yapmak istediginiz islemin numarasini giriniz"
+
+    print secenek1
+    print secenek2
+    print secenek3
+    print secenek4
+
+    soru = raw_input(":")
+
+    if soru == "1":
+        sayi1 = input("Ilk sayiyi girin")
+        sayi2 = input("Ikinci sayiyi girin")
+        print sayi1, "+", sayi2, "=", sayi1 + sayi2
+
+    if soru == "2":
+        sayi3 = input("Ilk sayiyi girin")
+        sayi4 = input("Ikinci sayiyi girin")
+        print sayi3, "-", sayi4, "=", sayi3 - sayi4
+
+    if soru == "3":
+        sayi5 = input("Ilk sayiyi girin")
+        sayi6 = input("Ikinci sayiyi girin")
+        print sayi5, "X", sayi6, "=", sayi5 * sayi6
+
+    if soru == "4":
+        sayi7 = input("Ilk sayiyi girin")
+        sayi8 = input("Ikinci sayiyi girin")
+        print sayi7, "/", sayi8, "=", sayi7 / sayi8
